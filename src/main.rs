1ec34bb20c24eee287ef2c724f9bcbfbd6e1b24c
Max Wong <110420743+Maxwe59@users.noreply.github.com>
2024-12-28 12:34:40 -0800

    project init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+/*
+components:
+Player(speed, health, size, global_pos, inventory)
+gun(range, fire rate, spread, )
+
+
+Other implimentations:
+Map rendering, collisions with objects
+
+
+*/
+
+
+/*
+DO TODAY:
+player movement (circle with no rotation),
+map movement and offset
+*/
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
