fd26578c70cef217350cfe5e6d1ae448e4e8c5aa
Max Wong <110420743+Maxwe59@users.noreply.github.com>
2024-12-28 12:39:09 -0800

    main bevy code

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-/*
+/* TODO:
 components:
 Player(speed, health, size, global_pos, inventory)
 gun(range, fire rate, spread, )
 
-
 Other implimentations:
 Map rendering, collisions with objects
-
-
 */
 
 
-/*
-DO TODAY:
+/* TODAY'S IMPLIMENTATIONS:
 player movement (circle with no rotation),
 map movement and offset
 */
 
-
+use bevy::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "shooter_2d".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
-
-
-
