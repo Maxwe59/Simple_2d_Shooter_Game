4a48304cde85d084dd5f8d7bc6e736aca0bd1540
Max Wong <110420743+Maxwe59@users.noreply.github.com>
2024-12-29 17:23:35 -0800

    map is rendered in chunks

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ Map rendering, collisions with objects
 player movement (circle with no rotation),
 map movement and offset
 */
-
-use bevy::prelude::*;
+use bevy::{color, prelude::*};
 use rand::Rng;
 
 #[derive(Component)]
@@ -22,8 +21,73 @@ struct Player {
 
 #[derive(Resource)]
 struct Map {
-    dimensions: Vec2,
-    //create a matrix for the map
+    grid_size: Vec2, //size of individual grid on the map
+    //partition -> how many rects are in a row, number of rows
+    partition: IVec2, //amount of individual grids on the map. 1 BASED COUNTING
+    //create a matrix for the map, stores coords for each rectangle
+    grid: Vec<Vec<Vec2>>, //[row][collumn]
+    dimensions: (f32, f32),
+}
+
+impl Map {
+    fn new_square(grid_length: f32, partition_sqre: i32) -> Self {
+        Map {
+            grid_size: Vec2 {
+                x: grid_length,
+                y: grid_length,
+            },
+            grid: Vec::new(),
+            partition: IVec2 {
+                x: partition_sqre,
+                y: partition_sqre,
+            },
+            dimensions: (
+                (grid_length * partition_sqre as f32),
+                (grid_length * partition_sqre as f32),
+            ),
+        }
+    }
+
+    fn generate_grid(&mut self) {
+        let mut tile_placement = Vec2 {
+            x: -((self.dimensions.0 - self.grid_size.x) / 2.0),
+            y: ((self.dimensions.1 - self.grid_size.y) / 2.0),
+        };
+        for _row in 0..self.partition.y {
+            let mut temp_vec: Vec<Vec2> = Vec::new();
+            for _collumn in 0..self.partition.x {
+                temp_vec.push(tile_placement);
+                tile_placement.x += self.grid_size.x;
+            }
+            self.grid.push(temp_vec);
+            tile_placement.y -= self.grid_size.y;
+            tile_placement.x =  -((self.dimensions.0 - self.grid_size.x) / 2.0);
+        }
+        //generates one single row of the grid
+        //should populate self.grid with coordinates for each rectangle
+    }
+}
+
+//spawn map
+fn spawn_map(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut map: ResMut<Map>,
+) {
+    
+    map.generate_grid();
+    for row in &map.grid {
+        for item in row {
+            commands.spawn((
+                //spawns in a single block from the map grid
+                Mesh2d(meshes.add(Rectangle::new(map.grid_size.x, map.grid_size.y))),
+                MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::WHITE))),
+                Transform::from_xyz(item.x, item.y, 0.0), //FIX LATER
+            ));
+        }
+        
+    }
 }
 
 fn spawn_player(
@@ -34,8 +98,8 @@ fn spawn_player(
 ) {
     //random coord generation
     let mut rng = rand::thread_rng();
-    let xrange = (map.dimensions.x/2.0) as i32;
-    let yrange = (map.dimensions.y/2.0) as i32;
+    let xrange = (map.dimensions.0 / 2.0) as i32;
+    let yrange = (map.dimensions.1 / 2.0) as i32;
     let rand_coords: Vec2 = Vec2 {
         x: (rng.gen_range(-xrange..xrange)) as f32,
         y: (rng.gen_range(-yrange..yrange)) as f32,
@@ -44,31 +108,16 @@ fn spawn_player(
     let player_global_pos = rand_coords;
     commands.spawn((
         Camera2d::default(),
-        Transform::from_xyz(player_global_pos.x, player_global_pos.y, 0.0),
+        Transform::from_xyz(player_global_pos.x, player_global_pos.y, 0.0), 
     ));
     commands.spawn((
-        Player {
-            speed: 200.0,
-        },
+        Player { speed: 200.0 },
         Mesh2d(meshes.add(Circle::new(30.0))),
         MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::BLACK))),
         Transform::from_xyz(0.0, 0.0, 1.0),
     ));
 }
 
-//spawn map
-fn spawn_map(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    map: Res<Map>,
-) {
-    commands.spawn((
-        Mesh2d(meshes.add(Rectangle::new(map.dimensions.x, map.dimensions.y))),
-        MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::WHITE))),
-        Transform::from_xyz(0.0, 0.0, 0.0),
-    ));
-}
 
 fn move_player(
     mut transform: ParamSet<(
@@ -78,17 +127,17 @@ fn move_player(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
     map: Res<Map>,
-    player: Query<&Player>
+    player: Query<&Player>,
 ) {
     //get player speed
     let mut speed: f32 = 0.0;
-    for item in player.iter(){
+    for item in player.iter() {
         speed = item.speed;
     }
 
     //displacement calculations
     let mut displacement: Vec3 = Vec3::ZERO;
-    let map_dimensions_half = (map.dimensions.x / 2.0, map.dimensions.y / 2.0);
+    let map_dimensions_half = (map.dimensions.0 / 2.0, map.dimensions.1 / 2.0);
 
     if keyboard_input.pressed(KeyCode::KeyD) {
         displacement.x += speed * time.delta_secs();
@@ -139,17 +188,11 @@ fn move_player(
         player_transform.translation = displacement;
         player_transform.translation.z = 1.0;
     }
-
 }
 
 fn main() {
     App::new()
-        .insert_resource(Map {
-            dimensions: Vec2 {
-                x: 1000.0,
-                y: 1000.0,
-            },
-        })
+        .insert_resource(Map::new_square(50.0, 10))
         .add_systems(Startup, spawn_map)
         .add_systems(Startup, spawn_player)
         .add_systems(Update, move_player)
